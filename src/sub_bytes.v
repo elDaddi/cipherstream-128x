a353b28fb446f0879d8d5dbccb034fd240c19a0c
Pratyush L Daddi <daddipratyush@gmail.com>
2026-06-26 20:07:56 +0530

    Update sub_bytes.v

--- src/sub_bytes.v
+++ src/sub_bytes.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.03.2026 17:23:13
-// Design Name: 
-// Module Name: sub_bytes
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module sub_bytes(
     input [127:0] data_in,
