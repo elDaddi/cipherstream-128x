c48d9257c35c22204630e62d4cd5d89acc44e14c
Pratyush L Daddi <daddipratyush@gmail.com>
2026-06-26 20:36:11 +0530

    Update round_0.v

--- src/round_0.v
+++ src/round_0.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.03.2026 19:01:05
-// Design Name: 
-// Module Name: round_0
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module round_0(
     input [127:0] data_in, round_key,
